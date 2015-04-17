66af4840400826495560e8321a3897fc321cefe2
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2015-04-17 10:49:58 +0200

    Updated javascript adapter

--- src/main/js/react/stjs-react.js
+++ src/main/js/react/stjs-react.js
@@ -3,15 +3,26 @@
 (function() {
     var oldExtend = stjs.extend;
 
-    stjs.extend = function() {
-        var initClass = oldExtend.apply(null, arguments);
+    stjs.extend = function(_constructor, _super, _implements, _initializer, _typeDescription, _annotations) {
 
-        if (stjs.getTypeAnnotation(initClass, "IsReactClass")) {
-            initClass = React.createClass(initClass.prototype);
-        } else if (stjs.getTypeAnnotation(initClass, "IsReactMixin")) {
-            initClass = React.createMixin(initClass.prototype);
+        // In this method, we go with the idea that a
+        // React Class never extends another one as
+        // Mixins should be used for the matter. Thus,
+        // we don't call extend when we encounter a React Class or Mixin
+        if (_annotations && _annotations._ && (_annotations._.IsReactClass || _annotations._.IsReactMixin) ) {
+            var method = (_annotations._.IsReactClass)? "createClass" : "createMixin",
+                statics = {},
+                proto = {};
+
+            // Use the STJS initializer to get the methods
+            if(typeof _initializer == "function") {
+                _initializer(statics, proto);
+                proto.statics = statics;
+            }
+
+            return React[method](proto);
         }
 
-        return initClass;
+        return oldExtend.apply(null, arguments);
     };
 })();
